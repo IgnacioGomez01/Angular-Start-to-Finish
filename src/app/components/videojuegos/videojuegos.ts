0304895b5c2839388f241dcd9bcc2a8c248fb254
IgnacioGomez01 <facundonacho@hotmail.com>
2025-08-22 05:22:53 -0300

    learn angular

---
+++ src/app/components/videojuegos/videojuegos.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-videojuegos',
+  imports: [FormsModule],
+  templateUrl: './videojuegos.html',
+  styleUrl: './videojuegos.css'
+})
+export class Videojuegos {
+  videojuegos: string[] = ["Apostol 1", "Apostol 2", "Apostol 3"];
+  nuevoJuego: string = "";
+  
+  addJuego() {
+    if(this.nuevoJuego && this.nuevoJuego.trim()){
+      this.videojuegos.push(this.nuevoJuego.trim());
+      this.nuevoJuego = "";
+    }
+  }
+
+}
